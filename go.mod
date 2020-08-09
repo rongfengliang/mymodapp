a63c11d55c015f83b7fd70f2caa35b35e6c7aac5
rong <1141591465@qq.com>
2020-08-09 10:12:17 +0800

    firrst

---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/rongfengliang/mymodapp
+
+go 1.14
+
---
+++ mymodapp.go
@@ -0,0 +1,12 @@
+package mymodapp
+
+// User user type
+type User struct {
+	Name     string
+	Password string
+}
+
+// Login login
+func (u User) Login() (string, error) {
+	return "dalong v2", nil
+}
